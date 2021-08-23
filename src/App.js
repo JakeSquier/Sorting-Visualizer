202325d4b9388de7c0194e2827a77297f5a8fc4e
github username <github email address>
2021-08-23 03:44:31 -0500

    first deploy push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Visualizer from './Visualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Visualizer />
+    </div>
+  );
+}
+
+export default App;
